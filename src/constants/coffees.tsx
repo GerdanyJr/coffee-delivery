93b6982cc4c99cd624f111db26e536aee4d80656
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-10 11:47:44 -0300

    feat: :sparkles: create coffees constant

---
+++ src/constants/coffees.tsx
@@ -0,0 +1,113 @@
+import { Coffee } from "@/@types/interface/coffee";
+
+export const COOFFES: Coffee[] = [
+  {
+    name: "Expresso Tradicional",
+    image: "/assets/img/Image-1.png",
+    tags: ["Tradicional"],
+    description: "O tradicional café feito com água quente e grãos moídos",
+    price: "9,90",
+  },
+  {
+    name: "Expresso Americano",
+    image: "/assets/img/Image-2.png",
+    tags: ["Tradicional", "Diluído"],
+    description: "Expresso diluído, menos intenso que o tradicional",
+    price: "9,90",
+  },
+  {
+    name: "Expresso Cremoso",
+    image: "/assets/img/Image-3.png",
+    tags: ["Tradicional", "Cremoso"],
+    description: "Café expresso tradicional com espuma cremosa",
+    price: "9,90",
+  },
+  {
+    name: "Expresso Gelado",
+    image: "/assets/img/Image-4.png",
+    tags: ["Tradicional", "Gelado"],
+    description: "Bebida preparada com café expresso e cubos de gelo",
+    price: "9,90",
+  },
+  {
+    name: "Café com Leite",
+    image: "/assets/img/Image-5.png",
+    tags: ["Tradicional", "Com Leite"],
+    description: "Meio a meio de espresso tradicional com leite vaporizado",
+    price: "9,90",
+  },
+  {
+    name: "Latte",
+    image: "/assets/img/Image-6.png",
+    tags: ["Com Leite"],
+    description:
+      "Uma dose de café expresso com o dobro de leite e espuma cremosa",
+    price: "9,90",
+  },
+  {
+    name: "Cappuccino",
+    image: "/assets/img/Image-7.png",
+    tags: ["Com Leite"],
+    description: "Bebida com canela feita de doses de café, leite e espuma",
+    price: "9,90",
+  },
+  {
+    name: "Macchiato",
+    image: "/assets/img/Type=Árabe.png",
+    tags: ["Com Leite"],
+    description:
+      "Café expresso misturado com um pouco de leite quente e espuma",
+    price: "9,90",
+  },
+  {
+    name: "Mocaccino",
+    image: "/assets/img/Type=Chocolate.png",
+    tags: ["Com Leite", "Especial"],
+    description: "Café expresso com calda de chocolate, pouco leite e espuma",
+    price: "9,90",
+  },
+  {
+    name: "Chocolate Quente",
+    image: "/assets/img/Type=Cubano.png",
+    tags: ["Especial"],
+    description: "Bebida feita com chocolate dissolvido no leite quente e café",
+    price: "9,90",
+  },
+  {
+    name: "Cubano",
+    image: "/assets/img/Type=Havaiano.png",
+    tags: ["Especial", "Alcoólico"],
+    description:
+      "Drink gelado de café expresso com rum, creme de leite e hortelã",
+    price: "9,90",
+  },
+  {
+    name: "Havaiano",
+    image: "/assets/img/Type=Irlandês.png",
+    tags: ["Especial", "Gelado"],
+    description: "Café expresso com leite condensado, gelo e coco ralado",
+    price: "9,90",
+  },
+  {
+    name: "Caramelo Macchiato",
+    image: "/assets/img/Type=Mochaccino.png",
+    tags: ["Com Leite", "Especial"],
+    description: "Café expresso com calda de caramelo e leite vaporizado",
+    price: "9,90",
+  },
+  {
+    name: "Espresso Doppio",
+    image: "/assets/img/Image-2.png",
+    tags: ["Tradicional", "Forte"],
+    description:
+      "Dose dupla de café espresso, ideal para quem gosta de um café forte",
+    price: "9,90",
+  },
+  {
+    name: "Ristretto",
+    image: "/assets/img/Image-3.png",
+    tags: ["Tradicional", "Concentrado"],
+    description: "Versão mais concentrada do espresso, com sabor mais intenso",
+    price: "9,90",
+  },
+];
