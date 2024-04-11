cc127c54e3538f3daa773d505fc1720e16bdb21d
GerdanyJr <125034782+GerdanyJr@users.noreply.github.com>
2024-04-11 15:29:37 -0300

    Revert "feat: :sparkles: organize checkout layout (#24)"
    This reverts commit 607d9f11fc41d8f3a8e9d1e2e9a3d041f2d812c7.

--- src/app/home/checkout/page.tsx
+++ src/app/home/checkout/page.tsx
@@ -1,17 +1,7 @@
 "use client";
-import { Payment } from "@/components/checkout/payment";
 import { CheckoutAddress } from "../../../components/checkoutAddress";
-import { OrderSummary } from "@/components/checkout/orderSummary";
-import styles from "./styles.module.css";
 
 export default function Checkout() {
-  return (
-    <div className={styles.container}>
-      <div className={styles.complete_container}>
-        <CheckoutAddress />
-        <Payment />
-      </div>
-      <OrderSummary />
-    </div >
-  );
+  return <CheckoutAddress />;
+
 }
