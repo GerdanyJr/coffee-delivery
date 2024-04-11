cbda04e2b654564b123ba37dd1fd97729a848f25
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-11 15:27:46 -0300

    feat: :sparkles: organize checkout layout

--- src/app/home/checkout/page.tsx
+++ src/app/home/checkout/page.tsx
@@ -1,7 +1,17 @@
 "use client";
+import { Payment } from "@/components/checkout/payment";
 import { CheckoutAddress } from "../../../components/checkoutAddress";
+import { OrderSummary } from "@/components/checkout/orderSummary";
+import styles from "./styles.module.css";
 
 export default function Checkout() {
-  return <CheckoutAddress />;
-
+  return (
+    <div className={styles.container}>
+      <div className={styles.complete_container}>
+        <CheckoutAddress />
+        <Payment />
+      </div>
+      <OrderSummary />
+    </div >
+  );
 }
