7eac606d721a26a9cadfebac33abc0caf772b7d5
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-11 15:23:45 -0300

    feat: :sparkles: add orderSummary navigation

--- src/components/checkout/orderSummary/index.tsx
+++ src/components/checkout/orderSummary/index.tsx
@@ -2,15 +2,17 @@
 import { ProductCart } from "@/components/UI/productCart";
 import styles from "./orderSummary.module.css";
 import { PrimaryButton } from "@/components/UI/primaryButton";
+import { useRouter } from "next/navigation";
 
 export function OrderSummary() {
+  const router = useRouter();
   return (
-    <>
+    <div>
       <h2 className={styles.summary_header}>Cafés selecionados</h2>
       <div className={styles.summary_container}>
         <div className={styles.items}>
-          <ProductCart />
-          <ProductCart />
+          <ProductCart name="Café expresso" price="9,90" />
+          <ProductCart name="Café expresso" price="9,90" />
         </div>
         <div className={styles.price}>
           <div>
@@ -26,8 +28,9 @@ export function OrderSummary() {
         <PrimaryButton
           label="Confirmar pedido"
           className={styles.confirm_button}
+          onClick={() => router.push("/home/success")}
         />
       </div>
-    </>
+    </div>
   );
 }
