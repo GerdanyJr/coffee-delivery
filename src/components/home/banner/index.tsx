c804b3532f07e424aa929113c32a76a6689e7b76
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-15 10:57:04 -0300

    refactor: :recycle: refactor banner styles to tailwind

--- src/components/home/banner/index.tsx
+++ src/components/home/banner/index.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import image from "@/../public/assets/img/Coffee.png";
-import styles from "./banner.module.css";
 import PurpleCooffee from "../../../../public/assets/svg/PurpleCooffee";
 import CartIcon from "../../../../public/assets/svg/CartIcon";
 import Box from "../../../../public/assets/svg/Box";
@@ -8,22 +7,25 @@ import Clock from "../../../../public/assets/svg/Clock";
 import { BannerItem } from "./bannerItem";
 
 export default function Banner() {
+  const itemsClasses = "flex flex-col gap-4";
   return (
-    <div className={styles.container}>
-      <div className={styles.brand_container}>
-        <div className={styles.brand}>
-          <h2>Encontre o café perfeito para qualquer hora do dia</h2>
-          <p>
+    <div className="flex justify-between items-center mt-24 gap-12">
+      <div>
+        <div>
+          <h2 className="text-5xl font-baloo font-bold mb-4">
+            Encontre o café perfeito para qualquer hora do dia
+          </h2>
+          <p className="font-roboto text-base-subtitle text-xl">
             Com o Cooffee Delivery você recebe seu café onde estiver, a qualquer
             hora
           </p>
         </div>
-        <div className={styles.items_container}>
-          <div className={styles.items}>
+        <div className="flex justify-between flex-wrap mt-16">
+          <div className={itemsClasses}>
             <BannerItem icon={<CartIcon />} title="Compra simples e segura" />
             <BannerItem icon={<Clock />} title="Entrega rápida e rastreada" />
           </div>
-          <div className={styles.items}>
+          <div className={itemsClasses}>
             <BannerItem
               icon={<PurpleCooffee />}
               title="O café chega fresquinho até você"
@@ -35,7 +37,7 @@ export default function Banner() {
           </div>
         </div>
       </div>
-      <img src={image.src} alt="A cooffee cup" />
+      <img src={image.src} alt="A cooffee cup" className="w-2/5" />
     </div>
   );
 }
--- src/components/home/header/location/index.tsx
+++ src/components/home/header/location/index.tsx
@@ -1,6 +1,4 @@
 import { MapPin } from "@phosphor-icons/react/dist/ssr";
-import styles from "./location.module.css";
-
 export default function Location({ place }: { place: string }) {
   return (
     <div className="bg-purple-light flex gap-1 p-1 items-center rounded-md">
