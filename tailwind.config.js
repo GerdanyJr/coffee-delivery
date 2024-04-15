9bedec615b6d438d33a9e4ba8f51a9ddb2aff70f
Larissa Moreira <76481905+laralis@users.noreply.github.com>
2024-04-15 13:47:24 -0300

    merge main into hml-larissa (#27)
    * chore: :bento: add banner assets
    
    * feat: :sparkles: create bannerItem component
    
    * feat: :sparkles: create banner component
    
    * test: :white_check_mark: create Banner tests
    
    * test: :white_check_mark: add badge tests to CartButton
    
    * feat: :sparkles: create the Option component
    
    * feat: :sparkles: add props to creditCardButton
    
    * feat: :sparkles: create options component
    
    * feat: :sparkles: create payment component
    
    * fix: :bug: fix the options component behavior
    
    * test: :white_check_mark: create the Option component tests
    
    * test: :white_check_mark: create the Options component tests
    
    * test: :white_check_mark: create the payment component tests
    
    * test: :white_check_mark: create credit Card Button tests
    
    * Feat/13 (#1)
    
    * test: :white_check_mark: create Header tests
    
    * test: :white_check_mark: add header tests
    
    * Feat/01 (#6)
    
    * refactor: :recycle: refactor ProductCard to accept props
    
    * feat: :sparkles: create coffees constant
    
    * feat: :sparkles: create productList component
    
    * chore: :package: change the productList directory
    
    * refactor: :recycle: refactor product list to accept items prop
    
    * test: :white_check_mark: create ProductCard tests
    
    * test: :white_check_mark: create the ProductList tests
    
    * test: :coffin: remove unused tests
    
    * fix: :adhesive_bandage: fix the product list header size
    
    * Test/UI components tests (#9)
    
    * test: :white_check_mark: create PrimaryButton tests
    
    * test: :white_check_mark: create ProductCart tests
    
    * refactor: :sparkles: refactor ProductCart to accept props
    
    * fix: :bug: fix the inputText onChange prop warning
    
    * test: :white_check_mark: create inputText tests
    
    * Feat/03 (#14)
    
    * fix: :adhesive_bandage: fix product cart width
    
    * fix: :bug: fix the primaryButton styling bug
    
    * feat: :sparkles: create the orderSummaryComponent
    
    * test: :white_check_mark: create OrderSummary test
    
    * fix: :adhesive_bandage: fix the badge size
    
    * Test/04/footer (#17)
    
    * feat: :sparkles: add new component footer
    
    * feat: :sparkles: add component footer
    
    * test: :test_tube: testing feature 04/footer
    
    ---------
    
    Co-authored-by: Lara Soluevo <larissa.lisboa@soluevo.com.br>
    
    * fix: :pencil2: fix typo in Option icons
    
    * feat: :lipstick: update line height in payment component header
    
    * Hotfix/layout bugs (#23)
    
    * fix: :bug: fix the header redirect bugs
    
    * fix: :adhesive_bandage: fix productCart layout
    
    * fix: :adhesive_bandage: fix checkoutAddress layout
    
    * feat: :sparkles: add orderSummary navigation
    
    * feat: :sparkles: add productList to home page
    
    * feat: :sparkles: organize checkout layout (#24)
    
    * build: :heavy_plus_sign: configure tailwind
    
    * fix: :pencil2: fix fonts in tailwind config
    
    ---------
    
    Co-authored-by: GerdanyJr <Asa_lindo@hotmail.com>
    Co-authored-by: GerdanyJr <125034782+GerdanyJr@users.noreply.github.com>
    Co-authored-by: Lara Soluevo <larissa.lisboa@soluevo.com.br>

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        yellow: {
+          DEFAULT: "#dbac2c",
+          dark: "#c47f17",
+          light: "#f1e9c9",
+        },
+        purple: {
+          DEFAULT: "#8047f8",
+          dark: "#4b2995",
+          light: "#ebe5f9",
+        },
+        base: {
+          title: "#272221",
+          subtitle: "#403937",
+          text: "#574f4d",
+          label: "#8d8686",
+          hover: "#d7d5d5",
+          button: "#e6e5e5",
+          input: "#ededed",
+          card: "#f3f2f2",
+        },
+        background: "#fafafa",
+        white: "#ffffff",
+      },
+      fontFamily: {
+        baloo: ["'Baloo 2'", "sans-serif"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
