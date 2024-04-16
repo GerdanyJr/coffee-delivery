7269e96775e4d52cf83c4bf43e6f1fedccf1e573
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-16 14:09:32 -0300

    fix: :bug: fix inputText layout bug

--- src/components/UI/inputText/index.tsx
+++ src/components/UI/inputText/index.tsx
@@ -19,12 +19,11 @@ export function InputText({
   return (
     <div className={`${className} relative inline`}>
       <input
-        className={`text-base-text bg-base-input p-4 rounded-lg border-2  border-transparent w-full focus:border-yellow-dark outline-none`}
+        className="w-full p-4 border-2 border-transparent rounded-lg outline-none text-base-text bg-base-input focus:border-yellow-dark"
         placeholder={placeholder}
         value={value}
         type={type ?? "text"}
         {...props}
-        className="p-4 border-2 border-transparent rounded-lg text-base-text bg-base-input focus:outline-none focus:border-yellow-dark"
       />
       {value?.length === 0 && !required && (
         <span className="absolute text-xs italic -translate-y-1/2 font-roboto top-1/2 right-3 text-base-label">
