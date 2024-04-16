67decdfa9ee7b34ad123af7fdc11f635d343e240
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-16 13:56:16 -0300

    fix: :bug: fix inputText opcional position

--- src/components/UI/inputText/index.tsx
+++ src/components/UI/inputText/index.tsx
@@ -24,7 +24,7 @@ export function InputText({
         className="p-4 border-2 border-transparent rounded-lg text-base-text bg-base-input focus:outline-none focus:border-yellow-dark"
       />
       {value?.length === 0 && !required && (
-        <span className="absolute text-sm italic font-sm font-roboto top-2/4 right-3 text-base-label translate-y-2/4">
+        <span className="absolute text-sm italic -translate-y-1/2 font-sm font-roboto top-1/2 right-3 text-base-label">
           Opcional
         </span>
       )}
