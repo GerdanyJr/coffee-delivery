90ca6d6842526e76524c7a1ec297e1818e266042
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-17 11:56:37 -0300

    feat: :sparkles: create arrowButton component

---
+++ src/components/home/banner/slider/arrowButton/index.tsx
@@ -0,0 +1,21 @@
+import { CaretCircleLeft, CaretCircleRight } from "@phosphor-icons/react";
+
+export function ArrowButton({
+  onClick,
+  left,
+}: {
+  onClick?: (e: any) => void;
+  left?: boolean;
+}) {
+  return (
+    <button
+      className={`absolute ${
+        left ? "left" : "right"
+      }-0 -translate-y-1/2 top-1/2`}
+      onClick={onClick}
+    >
+      {left && <CaretCircleLeft size={40} />}
+      {!left && <CaretCircleRight size={40} />}
+    </button>
+  );
+}
