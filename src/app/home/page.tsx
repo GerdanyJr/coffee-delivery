d0e2c47e185fa7fe5063fd5eda774fd4adbf4bf1
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-18 10:40:21 -0300

    feat: :sparkles: create mobileProductList component

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -6,15 +6,18 @@ import api from "@/services/api";
 import React, { useEffect, useState } from "react";
 import { Pages } from "@/@types/interface/pages";
 import Pagination from "../../components/home/pagination";
+import { useWindowDimensions } from "@/hooks/useWindowDimensions";
+import { MobileProductList } from "@/components/home/mobileProductList";
 
 export default function Home() {
+  const { width } = useWindowDimensions();
   const [products, setProducts] = useState<Coffee[]>([]);
   const [totalPages, setTotalPages] = useState(0);
   const [currentPage, setCurrentPage] = useState(1);
   useEffect(() => {
     async function loadProducts() {
       const response = await api.get("/coffee");
-      const data = (await response.data) as Pages;
+      const data = (await response.data) as Pages<Coffee>;
 
       setTotalPages(data.totalPages);
       setProducts(data.results);
@@ -31,12 +34,17 @@ export default function Home() {
   return (
     <>
       <Banner />
-      <ProductList title="Nossos cafés" products={products} />
-      <Pagination
-        totalPages={totalPages}
-        handleChangePage={handleChangePage}
-        currentPage={currentPage}
-      />
+      {width >= 768 && (
+        <>
+          <ProductList title="Nossos cafés" products={products} />
+          <Pagination
+            totalPages={totalPages}
+            handleChangePage={handleChangePage}
+            currentPage={currentPage}
+          />
+        </>
+      )}
+      {width < 768 && <MobileProductList title="Nossos cafés" />}
     </>
   );
 }
