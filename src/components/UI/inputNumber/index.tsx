d9c6adcf63271859c87fa0b68ad3bc2c0aa08a25
Lara Soluevo <larissa.lisboa@soluevo.com.br>
2024-04-22 08:42:41 -0300

    style: :lipstick: adding hover on input number component

--- src/components/UI/inputNumber/index.tsx
+++ src/components/UI/inputNumber/index.tsx
@@ -1,13 +1,13 @@
 import { Plus, Minus } from "@phosphor-icons/react";
 export function InputNumber() {
   return (
-    <div className="inline-flex bg-base-button gap-1 p-2 justify-center rounded-md h-[38px] items-center dark:bg-white">
-      <button className="w-4 h-8">
-        <Minus className="hover:fill-base-title" color="#8047F8" size={14} />
+    <div className="inline-flex bg-base-button gap-1  justify-center rounded-md h-[38px] items-center dark:bg-white">
+      <button className="w-full h-full py-1 pl-2 pr-1 group">
+        <Minus className="group-hover:fill-base-title" color="#8047F8" size={14} />
       </button>
-      <span className="m-1 leading-snug text-base-title">1</span>
-      <button className="w-4 h-8 ">
-        <Plus className="hover:fill-base-title" color="#8047F8" size={14} />
+      <span className="leading-snug text-base-title">1</span>
+      <button className="w-full h-full py-1 pl-1 pr-2 group">
+        <Plus className="group-hover:fill-base-title" color="#8047F8" size={14} />
       </button>
     </div>
   );
