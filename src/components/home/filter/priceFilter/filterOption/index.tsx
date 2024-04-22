411849355b68a37e139e5e5f95b570b790425f0b
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-22 17:34:37 -0300

    feat: :sparkles: create filterOption component

---
+++ src/components/home/filter/priceFilter/filterOption/index.tsx
@@ -0,0 +1,30 @@
+import { MouseEventHandler } from "react";
+
+export function FilterOption({
+  label,
+  value,
+  checked,
+  onClick,
+}: {
+  label: string;
+  value: string;
+  checked: boolean;
+  onClick: MouseEventHandler<HTMLInputElement>;
+}) {
+  return (
+    <div className="flex flex-row-reverse items-center font-light hover:underline">
+      <label htmlFor={value} className="pl-2 cursor-pointer peer">
+        {label}
+      </label>
+      <input
+        type="checkbox"
+        name="priceFilter"
+        checked={checked}
+        id={value}
+        value={value}
+        onClick={onClick}
+        className="cursor-pointer size-6 checked:accent-black"
+      />
+    </div>
+  );
+}
