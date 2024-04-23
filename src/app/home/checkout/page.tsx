e6ed0e91159e592ca07479ba07735ad8126478fc
Larissa Moreira <76481905+laralis@users.noreply.github.com>
2024-04-23 09:03:25 -0300

    feat: :sparkles: component SEO (#48)
    Co-authored-by: Lara Soluevo <larissa.lisboa@soluevo.com.br>

--- src/app/home/checkout/page.tsx
+++ src/app/home/checkout/page.tsx
@@ -1,16 +1,8 @@
-"use client";
-import { Payment } from "@/components/checkout/payment";
-import { CheckoutAddress } from "../../../components/checkoutAddress";
-import { OrderSummary } from "@/components/checkout/orderSummary";
-
+import CheckoutContent from "@/components/checkout/content";
+import { Metadata } from "next";
+export const metadata: Metadata = {
+  title: "Checkout",
+};
 export default function Checkout() {
-  return (
-    <div className="flex flex-col justify-between gap-8 mx-20 mt-20 lg:flex-row lg:gap-4">
-      <OrderSummary className="w-full lg:order-1" />
-      <div className="flex flex-col w-full gap-4">
-        <CheckoutAddress />
-        <Payment />
-      </div>
-    </div>
-  );
+  return <CheckoutContent />;
 }
--- src/app/home/success/page.tsx
+++ src/app/home/success/page.tsx
@@ -1,7 +1,11 @@
+import { Metadata } from "next";
 import { OrderInfo } from "../../../components/success/orderInfo";
+export const metadata: Metadata = {
+  title: "Success",
+};
 export default function Success() {
   return (
-    <div className='mx-20 lg:mx-0'>
+    <div className="mx-20 lg:mx-0">
       <OrderInfo />
     </div>
   );
