8daf1541e78a047cd151860e05ac00fef1aa6f6a
Lara Soluevo <larissa.lisboa@soluevo.com.br>
2024-04-23 08:45:10 -0300

    feat: :sparkles: component SEO

---
+++ src/components/checkout/content/index.tsx
@@ -0,0 +1,16 @@
+"use client";
+import { Payment } from "@/components/checkout/payment";
+import { CheckoutAddress } from "../../../components/checkoutAddress";
+import { OrderSummary } from "@/components/checkout/orderSummary";
+
+export default function CheckoutContent() {
+  return (
+    <div className="flex flex-col justify-between gap-8 mx-20 mt-20 lg:flex-row lg:gap-4">
+      <OrderSummary className="w-full lg:order-1" />
+      <div className="flex flex-col w-full gap-4">
+        <CheckoutAddress />
+        <Payment />
+      </div>
+    </div>
+  );
+}
