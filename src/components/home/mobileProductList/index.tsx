125042393f49c40147e8e76ff9561c9a3b87cb88
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-24 11:05:53 -0300

    feat: :sparkles: add priceFilter to mobileProductList

--- src/components/home/mobileProductList/index.tsx
+++ src/components/home/mobileProductList/index.tsx
@@ -4,12 +4,17 @@ import { Coffee } from "@/@types/interface/coffee";
 import { ProductCard } from "@/components/UI/productCard";
 import { useInfiniteScroll } from "@/hooks/useInfiniteScroll";
 import { getCoffees } from "@/services/coffeeService";
+import { PriceFilter } from "../filter/priceFilter";
 
 export function MobileProductList() {
-  const { data, ref } = useInfiniteScroll<Coffee>(getCoffees);
+  const { data, ref, filter, setFilter } =
+    useInfiniteScroll<Coffee>(getCoffees);
 
   return (
     <>
+      <div className="flex justify-end mx-8">
+        <PriceFilter filter={filter} setFilter={setFilter} />
+      </div>
       <h2 className="mx-2 mb-16 text-3xl font-bold text-center font-baloo text-base-subtitle">
         Nossos cafés
       </h2>
