4ab5836c00a9d74735250f3c87db7bdbb97e8ee5
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-26 11:02:20 -0300

    feat: :sparkles: create desktop tag filter

--- src/components/home/desktopProductList/index.tsx
+++ src/components/home/desktopProductList/index.tsx
@@ -2,15 +2,44 @@
 import { ProductList } from "../productList";
 import Pagination from "../pagination";
 import { PriceFilter } from "../filter/priceFilter";
-import { useProducts } from "@/hooks/useProducts";
+import { FilterType, useProducts } from "@/hooks/useProducts";
+import { useEffect, useState } from "react";
+import { Category } from "@/@types/interface/coffee";
+import { TagFilter } from "../filter/tagFilter";
 
 export function DesktopProductList() {
+  const [openedFilter, setOpenedFilter] = useState<FilterType>();
+  const [tags, setTags] = useState<Category[]>([]);
   const { filter, handleChangePage, page, products, setFilter, totalPages } =
     useProducts();
+  useEffect(() => {
+    async function getTags() {
+      const response = await fetch("http://localhost:8080/category");
+      const data = (await response.json()) as Category[];
+      setTags(data);
+    }
+    getTags();
+  }, []);
+
+  function onFilterPress(filter: FilterType) {
+    setOpenedFilter((prev) => (prev === filter ? undefined : filter));
+  }
   return (
     <>
-      <div className="flex items-center justify-end">
-        <PriceFilter filter={filter} setFilter={setFilter} />
+      <div className="flex justify-end gap-2">
+        <PriceFilter
+          filter={filter}
+          setFilter={setFilter}
+          open={openedFilter === "priceFilter"}
+          onPress={() => onFilterPress("priceFilter")}
+        />
+        <TagFilter
+          filter={filter.tags}
+          tags={tags}
+          open={openedFilter === "tagFilter"}
+          setFilter={setFilter}
+          onPress={() => onFilterPress("tagFilter")}
+        />
       </div>
       <h2 className="mx-2 mb-16 text-3xl font-bold text-start font-baloo text-base-subtitle">
         Nossos cafés
--- src/hooks/useProducts.tsx
+++ src/hooks/useProducts.tsx
@@ -1,11 +1,32 @@
 import { useState, useEffect, useCallback } from "react";
-import { Coffee } from "@/@types/interface/coffee";
+import { Category, Coffee } from "@/@types/interface/coffee";
 import { Pages } from "@/@types/interface/pages";
-import { Filter } from "@/components/home/filter/priceFilter";
 import api from "@/services/api";
 
+export interface Filter {
+  priceFilter: {
+    sort: string | undefined;
+    direction: "asc" | "desc" | undefined;
+    min: number | undefined;
+    max: number | undefined;
+  };
+  tags: Category[];
+}
+
+export const defaultFilter = {
+  priceFilter: {
+    sort: undefined,
+    direction: undefined,
+    min: undefined,
+    max: undefined,
+  },
+  tags: [],
+};
+
+export type FilterType = "tagFilter" | "priceFilter" | undefined;
+
 export function useProducts() {
-  const [filter, setFilter] = useState<Filter | undefined>();
+  const [filter, setFilter] = useState<Filter>(defaultFilter);
   const [products, setProducts] = useState<Coffee[]>([]);
   const [totalPages, setTotalPages] = useState(0);
   const [page, setPage] = useState(1);
@@ -17,11 +38,19 @@ export function useProducts() {
   const handleChangePage = useCallback(
     async (currentPage: number) => {
       let url = `/coffee?page=${currentPage - 1}`;
-      for (const attr in filter) {
-        if (filter[attr]) {
-          url += `&${attr}=${filter[attr]}`;
+      for (const attr in filter.priceFilter) {
+        if (filter.priceFilter[attr as keyof typeof filter.priceFilter]) {
+          url += `&${attr}=${
+            filter.priceFilter[attr as keyof typeof filter.priceFilter]
+          }`;
         }
       }
+      if (filter.tags.length > 0) {
+        const tagIds = [...filter.tags.map((tag) => tag.id)];
+        url = url.replace("/coffee", url.split("?")[0] + "/category");
+        url += `&tags=${tagIds.join(",")}`;
+      }
+
       const response = await api.get(url);
       const page = (await response.data) as Pages<Coffee>;
 
@@ -29,8 +58,22 @@ export function useProducts() {
       setTotalPages(page.totalPages);
       setPage(page.pageNumber + 1 > page.totalPages ? 1 : page.pageNumber + 1);
     },
-    [page, filter?.direction, filter?.max, filter?.min, filter?.sort]
+    [
+      page,
+      filter.priceFilter.direction,
+      filter.priceFilter.max,
+      filter.priceFilter.min,
+      filter.priceFilter.sort,
+      filter.tags,
+    ]
   );
 
-  return { products, totalPages, page, filter, handleChangePage, setFilter };
+  return {
+    products,
+    totalPages,
+    page,
+    filter,
+    handleChangePage,
+    setFilter,
+  };
 }
