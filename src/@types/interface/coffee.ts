a997e06fa549deaf4126ffc60623a7cfbf3efe6c
GerdanyJr <Asa_lindo@hotmail.com>
2024-04-26 10:47:34 -0300

    feat: :sparkles: create tag filter component

--- src/@types/interface/coffee.ts
+++ src/@types/interface/coffee.ts
@@ -1,8 +1,12 @@
 export interface Coffee {
   id: string;
   image: string;
-  categories: [{ name: string; id: string }];
+  categories: Category[];
   name: string;
   description: string;
   price: string;
+}
+export interface Category {
+  name: string;
+  id: string;
 }
